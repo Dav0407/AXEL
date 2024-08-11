ca07a0f8b95516a4d6c80fb3c9ec1a21c87aa62f
Davronbek Normamatov <davronnormamatov004@gmail.com>
2024-08-11 14:36:59 +0500

    version 1.0.0

---
+++ src/main/java/com/igriss/AXEL/AxelApplication.java
@@ -0,0 +1,13 @@
+package com.igriss.AXEL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igriss/AXEL/AxelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igriss.AXEL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
